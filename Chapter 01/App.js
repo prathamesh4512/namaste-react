ec97679bdfafabe3c2f446c1bf8561cd2ba58a45
prathamesh4512 <prathamesh4512@gmail.com>
2024-11-29 13:44:37 +0530

    Chapter 01

---
+++ Chapter 01/App.js
@@ -0,0 +1,44 @@
+const Heading = React.createElement("h1",{id:"heading", temp:"xyz"},"Hello Word")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(Heading)
+
+console.log(Heading)
+
+/**
+ * <div id="container">
+ * <h1 id="title" style={background:"red"}>heading</h1>
+ * <h1 id="title">heading1</h1>
+ * </div>
+ * 
+ */
+
+// Create nested React Elements
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+      style: {
+        background:"red",
+      },
+      className:"title"
+    },
+    "heading"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
